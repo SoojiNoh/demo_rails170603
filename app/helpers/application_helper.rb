750eea84feee4b20bbd37a4df1f18b77ec5b87ee
soojinoh <thensj@hanmail.net>
2017-07-02 22:21:17 +0000

    change nested_row into row_nested

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
         link_to(name, '#', class: "add_fields " + args[:class], data: {id: id, fields: fields.gsub("\n",""), label: args[:label]}) 
     end
     
-    def link_to_add_nested_row(name, f, association, **args)
+    def link_to_add_row_nested(name, f, association, **args)
         new_object = f.object.send(association).klass.new
         id = new_object.object_id
         fields = f.simple_fields_for(association, new_object, child_index: id) do |builder|

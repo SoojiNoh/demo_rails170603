abf183f7ad1a871fabf8c2d6a492548d8a292122
soojinoh <thensj@hanmail.net>
2017-08-17 08:12:59 +0000

    fix catalogue_current bug

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,9 +4,10 @@ class DashboardsController < ApplicationController
     @catalogues = current_user.catalogues
     @catalogue_latest = @catalogues? @catalogues.last : nil
     @exhibition_latest = @catalogue_latest? @catalogue_latest.exhibitions.first : nil
-    if @exhibition && @exhibition_latest.start_date < Date.today && Date.today < @exhibition_latest.end_date
+    if @exhibition_latest && @exhibition_latest.start_date < Date.today && Date.today < @exhibition_latest.end_date
       @catalogue_current = @catalogue_latest
       @exhibition_current = @exhibition_latest
+      puts @catalogue_current
     else
       @catalogue_current = nil
       @exhibition_current = nil
